564e4a9c2105bc37581d8166585956e947e0a426
momothemonster <momodmonster@gmail.com>
2018-02-18 18:09:05 -0800

    Add Rigged Monster

---
+++ Assets/_Vendor/MMM/Tools/Editor/Anima2DHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MMM.Tools
+{
+
+    public class Anima2DHelper : EditorWindow
+    {
+        [MenuItem("Tools/Add Bone %&b")]
+        internal static void AddBone()
+        {
+            Debug.LogFormat("AddBone");
+            GameObject go = new GameObject("Bone");
+            if (Selection.activeTransform != null)
+            {
+                go.transform.parent = Selection.activeTransform;
+                go.transform.position = Selection.activeTransform.position;
+                go.AddComponent<Anima2D.Bone2D>();
+            }
+        }
+    }
+}
